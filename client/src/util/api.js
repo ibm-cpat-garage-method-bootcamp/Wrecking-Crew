72237ffb53e9dc5b4ecdc367b8d2711439047a58
Patrick Gerspach <patrick.gerspach@ibm.com>
2020-01-16 15:08:23 -0600

    Use local storage to persist data on mock api calls

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -76,14 +76,29 @@ const defaultCatalogItems = [
     }
 ];
 
+if(!window.localStorage.getItem('catalogItems')) {
+  window.localStorage.setItem('catalogItems', JSON.stringify(defaultCatalogItems));
+}
+if(!window.localStorage.getItem('shoppingListItems')) {
+  window.localStorage.setItem('shoppingListItems', JSON.stringify(defaultShoppingListItems));
+}
+
 async function getShoppingListItems(){
+  if(window.localStorage.getItem('shoppingListItems')) {
+    return JSON.parse(window.localStorage.getItem('shoppingListItems'));
+  } 
     return defaultShoppingListItems;
 }
 async function getCatalogItems(){
+  if(window.localStorage.getItem('catalogItems')) {
+    return JSON.parse(window.localStorage.getItem('catalogItems'));
+  } 
     return defaultCatalogItems;
 }
 async function addCatalogItem(item){
-  return defaultCatalogItems.concat([item])
+  const currentCatalogItems = JSON.parse(window.localStorage.getItem('catalogItems'));
+  window.localStorage.setItem('catalogItems', JSON.stringify([...currentCatalogItems, item]))
+  return currentCatalogItems.concat([item])
 }
 
 export {
